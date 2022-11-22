b776affc1168a94fe574211315606ef8fab00ca8
rt <florentino@ua.pt>
2022-11-22 13:36:41 +0000

    test button suspends other tasks

--- assignment3/distance_detector_system/src/main.c
+++ assignment3/distance_detector_system/src/main.c
@@ -1,13 +1,3 @@
-/*
- * Paulo Pedreiras, 2022/10
- * Zephyr: Simple thread creation example (2)
- *
- * One of the tasks is periodc, the other two are activated via a semaphore. Data communicated via sharem memory
- *
- * Base documentation:
- *      https://developer.nordicsemi.com/nRF_Connect_SDK/doc/latest/zephyr/reference/kernel/index.html
- *
- */
 
 #include <zephyr.h>
 #include <device.h>
@@ -94,12 +84,12 @@ k_tid_t thread_output_tid;
 k_tid_t thread_button_tid;
 
 /* Global vars (shared memory between tasks A/B and B/C, resp) */
-uint16_t ab = 100;
-uint16_t bc = 200;
+uint16_t sensor_processing = 100;
+uint16_t processing_output = 200;
 
 /* Semaphores for task synch */
-struct k_sem sem_ab;
-struct k_sem sem_bc;
+struct k_sem sem_sensor_processing;
+struct k_sem sem_processing_output;
 struct k_sem sem_button;
 
 /* Thread code prototypes */
@@ -142,7 +132,9 @@ static int adc_sample(void)
 /* Define a callback function. It is like an ISR that is called when the button is pressed */
 void button_pressed(const struct device *dev, struct gpio_callback *cb, uint32_t pins)
 {
+
     k_sem_give(&sem_button);
+    // k_sem_take(&sem_button,K_NO_WAIT);
 }
 
 /* Main function */
@@ -225,8 +217,8 @@ void main(void)
     printf("\n\r Illustration of the use of shmem + semaphores\n\r");
 
     /* Create and init semaphores */
-    k_sem_init(&sem_ab, 0, 1);
-    k_sem_init(&sem_bc, 0, 1);
+    k_sem_init(&sem_sensor_processing, 0, 1);
+    k_sem_init(&sem_processing_output, 0, 1);
     k_sem_init(&sem_button, 0, 1);
 
     /* Create tasks */
@@ -239,13 +231,13 @@ void main(void)
                                             NULL, NULL, NULL, thread_processing_prio, 0, K_NO_WAIT);
 
     thread_output_tid = k_thread_create(&thread_output_data, thread_output_stack,
-                                            K_THREAD_STACK_SIZEOF(thread_output_stack), thread_output_code,
-                                            NULL, NULL, NULL, thread_output_prio, 0, K_NO_WAIT);
+                                        K_THREAD_STACK_SIZEOF(thread_output_stack), thread_output_code,
+                                        NULL, NULL, NULL, thread_output_prio, 0, K_NO_WAIT);
 
     thread_button_tid = k_thread_create(&thread_button_data, thread_button_stack,
-                                            K_THREAD_STACK_SIZEOF(thread_button_stack), thread_button_code,
-                                            NULL, NULL, NULL, thread_button_prio, 0, K_NO_WAIT);
-   return;
+                                        K_THREAD_STACK_SIZEOF(thread_button_stack), thread_button_code,
+                                        NULL, NULL, NULL, thread_button_prio, 0, K_NO_WAIT);
+    return;
 }
 
 /* Thread code implementation */
@@ -257,7 +249,7 @@ void thread_sensor_code(void *argA, void *argB, void *argC)
     /* Other variables */
     long int nact = 0;
 
-    printk("Thread A init (periodic)\n");
+    printk("Thread sensor init (periodic)\n");
 
     /* Compute next release instant */
     release_time = k_uptime_get() + SAMP_PERIOD_MS;
@@ -268,7 +260,7 @@ void thread_sensor_code(void *argA, void *argB, void *argC)
     {
 
         /* Do the workload */
-        printk("\n\nThread A instance %ld released at time: %lld (ms). \n", ++nact, k_uptime_get());
+        printk("\n\nThread sensor instance %ld released at time: %lld (ms). \n", ++nact, k_uptime_get());
 
         err = adc_sample();
         if (err)
@@ -285,15 +277,20 @@ void thread_sensor_code(void *argA, void *argB, void *argC)
             {
                 /* ADC is set to use gain of 1/4 and reference VDD/4, so input range is 0...VDD (3 V), with 10 bit resolution */
                 printk("adc reading: raw:%4u / %4u mV: \n\r", adc_sample_buffer[0], (uint16_t)(1000 * adc_sample_buffer[0] * ((float)3 / 1023)));
-                ab = (uint16_t)(1000 * adc_sample_buffer[0] * ((float)3 / 1023));
-                printk("Thread A set ab value to: %d \n", ab);
+                sensor_processing = (uint16_t)(1000 * adc_sample_buffer[0] * ((float)3 / 1023));
+                printk("Thread sensor set sensor_processing value to: %d \n", sensor_processing);
             }
         }
 
-        k_sem_give(&sem_ab);
+        k_sem_give(&sem_sensor_processing);
 
         /* Wait for next release instant */
         fin_time = k_uptime_get();
+
+        printk("fin time=  %lld, release_time= %lld, sub=%lld", fin_time, release_time,release_time-fin_time );
+        if(fin_time - release_time > SAMP_PERIOD_MS){
+            release_time += SAMP_PERIOD_MS*5;
+        }
         if (fin_time < release_time)
         {
             k_msleep(release_time - fin_time);
@@ -310,20 +307,20 @@ void thread_processing_code(void *argA, void *argB, void *argC)
     uint16_t current_read = 0;
     int iterations = 0;
 
-    printk("Thread B init (sporadic, waits on a semaphore by task A)\n");
+    printk("Thread processing  init (sporadic, waits on a semaphore by task A)\n");
     while (1)
     {
-        k_sem_take(&sem_ab, K_FOREVER);
+        k_sem_take(&sem_sensor_processing, K_FOREVER);
 
-        printk("Thread B instance %ld released at time: %lld (ms). \n", ++nact, k_uptime_get());
-        printk("Task B read ab value: %d\n", ab);
-        current_read = ab;
+        printk("Thread processing  instance %ld released at time: %lld (ms). \n", ++nact, k_uptime_get());
+        printk("Task processing read sensor_processing value: %d\n", sensor_processing);
+        current_read = sensor_processing;
 
         if (iterations > 8)
         {
             uint16_t avg = precedentsAverage(precedents, current_read, 9);
-            bc = avg;
-            k_sem_give(&sem_bc);
+            processing_output = avg;
+            k_sem_give(&sem_processing_output);
             printk("avg([%d,%d,%d,%d,%d,%d,%d,%d,%d,%d]]) = %d\n", precedents[0], precedents[1], precedents[2], precedents[3], precedents[4], precedents[5], precedents[6], precedents[7], precedents[8], current_read, avg);
 
             precedents[iterations % 9] = current_read;
@@ -331,7 +328,7 @@ void thread_processing_code(void *argA, void *argB, void *argC)
         else
             precedents[iterations] = current_read;
         iterations++;
-        printk("Thread B set bc value to: %d \n", bc);
+        printk("Thread processing  set processing_output value to: %d \n", processing_output);
     }
 }
 
@@ -341,15 +338,15 @@ void thread_output_code(void *argA, void *argB, void *argC)
     long int nact = 0;
     int ret;
 
-    printk("Thread C init (sporadic, waits on a semaphore by task A)\n");
+    printk("Thread output init (sporadic, waits on a semaphore by task A)\n");
 
     while (1)
     {
-        k_sem_take(&sem_bc, K_FOREVER);
-        printk("Thread C instance %5ld released at time: %lld (ms). \n", ++nact, k_uptime_get());
-        printk("Task C read bc value: %d\n", bc);
+        k_sem_take(&sem_processing_output, K_FOREVER);
+        printk("Thread output instance %5ld released at time: %lld (ms). \n", ++nact, k_uptime_get());
+        printk("Task output read processing_output value: %d\n", processing_output);
 
-        if (bc < 1000)
+        if (processing_output < 1000)
         {
             ret = gpio_pin_set_dt(&led0, 1);
             if (ret < 0)
@@ -364,7 +361,7 @@ void thread_output_code(void *argA, void *argB, void *argC)
             if (ret < 0)
                 printk("SETTING LED VALUE FAILED");
         }
-        else if (bc < 2000)
+        else if (processing_output < 2000)
         {
             ret = gpio_pin_set_dt(&led0, 1);
             if (ret < 0)
@@ -379,7 +376,7 @@ void thread_output_code(void *argA, void *argB, void *argC)
             if (ret < 0)
                 printk("SETTING LED VALUE FAILED");
         }
-        else if (bc < 3000)
+        else if (processing_output < 3000)
         {
             ret = gpio_pin_set_dt(&led0, 1);
             if (ret < 0)
@@ -422,6 +419,11 @@ void thread_button_code(void *argA, void *argB, void *argC)
     while (1)
     {
         k_sem_take(&sem_button, K_FOREVER);
+
+        k_thread_suspend(thread_sensor_tid);
+        k_thread_suspend(thread_processing_tid);
+        k_thread_suspend(thread_output_tid);
+
         ret = gpio_pin_set_dt(&led0, 0);
         if (ret < 0)
             printk("SETTING LED VALUE FAILED");
@@ -444,11 +446,12 @@ void thread_button_code(void *argA, void *argB, void *argC)
             gpio_pin_toggle_dt(&led3);
             k_msleep(SLEEP_TIME_MS);
         }
-        
+
+        k_thread_resume(thread_sensor_tid);
+        k_thread_resume(thread_processing_tid);
+        k_thread_resume(thread_output_tid);
     }
 }
-
-
 
 int precedentsAverage(uint16_t *precedents, uint16_t current_read, uint16_t precedents_size)
 {
