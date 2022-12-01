7b1a65465151ee6a7e116d2dbf3043e12eecab0d
gilteixeira <gilteixeira@ua.pt>
2022-12-01 19:59:25 +0000

    added cool way to generate a gif of the images

--- project/imageVisualizer.py
+++ project/imageVisualizer.py
@@ -1,6 +1,14 @@
 from PIL import Image
-import io
-with open("imageBib/vertical", "rb") as imageFile:
-    f = imageFile.read()
-    image = Image.frombytes('L', (128,128), bytes(f))
-    image.show()
+import imageio
+
+images = []
+for i in range(1,100):
+    with open("images/img" + str(i) + ".raw", "rb") as imageFile:
+        images.append(Image.frombytes('L', (128,128), bytes(imageFile.read())))
+imageio.mimsave('imagesGif.gif', images)
+
+
+#with open("imageBib/vertical", "rb") as imageFile:
+#    f = imageFile.read()
+#    image = Image.frombytes('L', (128,128), bytes(f))
+#    image.show()
