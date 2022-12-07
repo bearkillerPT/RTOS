ab1383248d7cb5dbc45b9035765dd9f2759c3153
gilteixeira <gilteixeira@ua.pt>
2022-12-07 17:58:11 +0000

    new images to escape objects

--- project/generateImages.py
+++ project/generateImages.py
@@ -25,24 +25,29 @@ with open("imageBib/right23", "rb") as imageFile:
 with open("imageBib/right64", "rb") as imageFile:
     image_set[6] = np.fromfile(imageFile, dtype=np.uint8)
 
-object_x = 70
-object_size = 20
-
+object_x = 45
+object_width = 20
+object_height = 20
 direction_choice = 0
 if len([x for x in image_set if x is not None]) == len(image_set):
     for i in range(1, 100):
-        direction_choice = random.randint(0, 6)
-
+        if i < 30:
+            direction_choice = random.randint(0, 6)
+        else:
+            direction_choice = random.randint(3, 6)
         with open("images/img" + str(i) + ".raw", "wb") as binary_file:
             selected_image = copy.deepcopy(
                 image_set[direction_choice].reshape(128, 128))
             object_y = i + 20
             if object_y > 0 and object_y < 128:
-                selected_image[i:i+object_size,
-                            object_x:object_x+object_size] = 128
-            if direction_choice == 0:
-                object_x -= 1 
-            elif direction_choice == 2:
-                object_x += 1 
+                selected_image[i:i+object_height,
+                               object_x:object_x+object_width] = 128
+            # if direction_choice < 2:
+            #    object_x += 1 + direction_choice
+            if direction_choice > 2:
+                if object_x > 0:
+                    object_x -= 1
+                elif object_width > 0:
+                    object_width -= 1
             binary_file.write(bytes(bytearray(selected_image)))
     # Generate image
